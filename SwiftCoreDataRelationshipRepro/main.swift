7ac8d45ecd66c0bd16a0223fe00849f2f0c7d87c
rentzsch <jwr.git@redshed.net>
2014-07-10 15:35:32 -0500

    Initial Commit

---
+++ SwiftCoreDataRelationshipRepro/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftCoreDataRelationshipRepro
+//
+//  Created by Wolf Rentzsch on 7/10/14.
+//  Copyright (c) 2014 Jonathan 'Wolf' Rentzsch. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
